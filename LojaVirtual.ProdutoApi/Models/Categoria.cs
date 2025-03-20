62fcc522d1fd040017a2f35f716940f53ce4d04d
adryano.chavier <adryano.chavier@senac.br>
2025-03-20 15:31:10 -0300

    Models

---
+++ LojaVirtual.ProdutoApi/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace LojaVirtual.ProdutoApi.Models;
+public class Categoria
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
+
+}
---
+++ LojaVirtual.ProdutoApi/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace LojaVirtual.ProdutoApi.Models;
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public string? Descricao { get; set; }
+    public long Estoque {  get; set; }
+    public string? ImagemURL {  get; set; }
+    public Categoria? Categoria { get; set; }
+    public int CategoriaId { get; set; }
+
+}
